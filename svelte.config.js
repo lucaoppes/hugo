095ee4bcdb8858d70f199a644e79560e0f10f7e9
Luca <oppes.luca1@gmail.com>
2025-12-08 15:01:06 +0000

    yeah

--- svelte.config.js
+++ svelte.config.js
@@ -16,7 +16,7 @@ const config = {
 			strict: true
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : '/lucaoppes.github.io/hugo'
+			base: process.argv.includes('dev') ? '' : '/hugo'
 		}
 	}
 };
