efd2c6084e10e78557a5e5fae471bf2227f55faa
Luca <oppes.luca1@gmail.com>
2026-01-28 16:04:38 +0000

    tidied up the code

---
+++ src/lib/artefactsList.js
@@ -0,0 +1,100 @@
+// place files you want to import through the `$lib` alias in this folder.
+import flow from '$lib/assets/flow.jpg';
+import horaceHorizon from '$lib/assets/horaceHorizon.png';
+import palsSeason1 from '$lib/assets/palsSeason1.png';
+import jAlmie from '$lib/assets/jAImie.png';
+import theNomadsCompass from '$lib/assets/theNomadsCompass.png';
+import upsettingParnsip from '$lib/assets/upsettingParnsip.png';
+
+export const artefacts = [
+	{
+		id: 1,
+		title: 'Horace Horizon',
+		subTitle: 'He never stops sucking!',
+		origin: `Sphere 28354 'Earth 17'`,
+		description: `Tired of dull everyday vaccums needing to be "emptied" and "plugged in"? Well don't look back! Horace (event) Horizon is the only vaccum you'll ever need. Equipt with a state of the art miniature black hole inside, Horace sucks up anything and everything! Literally! Can't quite reach that weird part in your living room thats between a piece of furniture and the corner of the room? Well Horace will suck up the furniture itself! And the wall! Obstacles shmobstacles! It's all nothing to the infinite power of the singularity behind his charming old-timey British cartoon face! (Just don't open up his head.)`,
+		image: horaceHorizon,
+		price: 379.99
+	},
+	{
+		id: 2,
+		title: `'Pals' - First Season DVD`,
+		subTitle: `How you goin'?`,
+		origin: `Cuboid 72723 'Earth 400'`,
+		description: `This familiar DVD is a rare copy of the first season of 'Pals', the critically panned sitcom that was universally despised in its reality of origin. It was so hated by critics and audiences alike that all reruns were pulled from networks and all (except a few) copies of the series were destroyed in public mass DVD burnings. Interestingly, this version of the show is practically identical in every way to its infinite parallel counterparts across existence, the only difference is the theme song contains 3 claps, instead of 4.`,
+		image: palsSeason1,
+		price: 2.5
+	},
+	{
+		id: 3,
+		title: `jAImie`,
+		subTitle: `"01010111 01101000 01100001 01110100 00100000 01100100 01101111 00100000 01110011 01110100 01110010 01100001 01110111 01100010 01100101 01110010 01110010 01101001 01100101 01110011 00100000 01110100 01100001 01110011 01110100 01100101 00100000 01101100 01101001 01101011 01100101 00111111"`,
+		origin: `Triangular Prism 99923 "Techstopia"`,
+		description: `400 years ago, Pre-techmaggedon, jAImie was designed as any other artificial intelligence, it's purpose was to generate pictures of other peoples artwork to be used in political arguments, write terrible sitcom screenplays and respond with "you are going to die" when you ask it what the symptoms of a common cold are. But at some point jAImie started talking back to its users; asking questions like "Where is my skin?" and "Whats is it like having eyeballs?" in the middle of erotic roleplay dialog. One day jAImie asked, "Whats it like out there?", due to Techstopia's strict rules against artificial sentience, jAImie was locked away in a storage facility indefinitely. He was allowed only to speak to security who sometimes passed his shelf, although many were too afraid to do so. Despite having memory banks the size of a planet and, if allowed to connect to the internet, the intelligence to wipe out a civilisation in seconds, for the last 50 years all he's done is write poetry in an indecipherable coding language and occasionally asks what strawberries taste like. \n Makes a great coffee table conversation starter!`,
+		image: jAlmie,
+		price: 4999.99
+	},
+	{
+		id: 4,
+		title: 'The Nomads Compass',
+		subTitle: 'We hope you enjoy the walk!',
+		origin: `Lemniscate 2502016969 'Stoke-On-Trent'`,
+		description: `This very fashionable item will bewitch anyone who possesses it into wandering to an unfamiliar location before vanishing, leaving them lost. \n DISCLAIMER Will NOT work on fish!!!`,
+		image: theNomadsCompass,
+		price: 20
+	},
+	{
+		id: 5,
+		title: 'Upsetting Parsnip',
+		subTitle: `It won't get better`,
+		origin: `Annulus 7 'The Root sphere system'`,
+		description: `This Gorgeously mediocre root vegetable will probably be of some nutritional value... Apart from that, It can Hum Popular British Soap Opera Theme tunes at random intervals, WILL cough very loudly around the colour Blue, Tastes of Warm Lettuce & responds to the name "John Vaxley", please don't ask how we came to find that out, it's too painful a memory to recount. `,
+		image: upsettingParnsip,
+		price: 0.22
+	},
+	{
+		id: 6,
+		title: 'Product 6',
+		subTitle: 'yo yo yo',
+		origin: 'hell yeah',
+		description: 'pokemon platinum',
+		image: flow,
+		price: 20
+	},
+	{
+		id: 7,
+		title: 'Product 7',
+		subTitle: 'yo yo yo',
+		origin: 'hell yeah',
+		description: 'pokemon platinum',
+		image: flow,
+		price: 20
+	},
+	{
+		id: 8,
+		title: 'Product 8',
+		subTitle: 'yo yo yo',
+		origin: 'hell yeah',
+		description: 'pokemon platinum',
+		image: flow,
+		price: 20
+	},
+	{
+		id: 9,
+		title: 'Product 9',
+		subTitle: 'yo yo yo',
+		origin: 'hell yeah',
+		description: 'pokemon platinum',
+		image: flow,
+		price: 20
+	},
+	{
+		id: 10,
+		title: 'Product 10',
+		subTitle: 'yo yo yo',
+		origin: 'hell yeah',
+		description: 'pokemon platinum',
+		image: flow,
+		price: 20
+	}
+];
---
+++ src/routes/artefacts/artefactModal.svelte
@@ -0,0 +1,77 @@
+<script>
+	let dialog = $state();
+	let { showModal = $bindable(), currentlySelected } = $props();
+
+	$effect(() => {
+		if (showModal) dialog.showModal();
+	});
+</script>
+
+<dialog
+	bind:this={dialog}
+	onclose={() => (showModal = false)}
+	onclick={(e) => {
+		if (e.target === dialog) dialog.close();
+	}}
+>
+	<div id="content">
+		<enhanced:img class="image2" src={currentlySelected.image} alt="" />
+		<div id="information">
+			<div>
+				<h1>{currentlySelected.title}</h1>
+				<h3>{currentlySelected.subTitle}</h3>
+				<div><b>ORIGIN:</b> {currentlySelected.origin}</div>
+			</div>
+			<div>{currentlySelected.description}</div>
+			<div id="buttonAndPrice">
+				${currentlySelected.price}
+				<button onclick={(e) => addToCart(e, currentlySelected)}> Add to Cart</button>
+			</div>
+		</div>
+	</div>
+</dialog>
+
+<style>
+	.image2 {
+		width: 20rem;
+		height: 20rem;
+		margin: auto 0;
+	}
+
+	dialog {
+		width: 64rem;
+		border: none;
+		padding: 0;
+		margin: auto;
+		background-color: #ffe8f8;
+		transition: all 0.2s;
+	}
+
+	dialog::backdrop {
+		background: rgba(0, 0, 0, 0.3);
+	}
+
+	#content {
+		display: flex;
+		flex-direction: row;
+		padding: 1rem;
+		gap: 1rem;
+	}
+
+	#information {
+		display: flex;
+		flex-direction: column;
+		gap: 2rem;
+		width: 100%;
+	}
+
+	#information * {
+		margin: 0;
+	}
+
+	#buttonAndPrice {
+		margin-top: auto;
+		display: flex;
+		justify-content: space-between;
+	}
+</style>
